e8161eab80aa19188394882fe020a75b32da6bd8
UxueAS <uxue.anabitarte838@comunidadunir.net>
2024-12-26 12:27:17 +0100

    imagenes arte

--- src/assets/images.js
+++ src/assets/images.js
@@ -7,8 +7,30 @@ import Arzak from './sorteos/arzak.jpg';
 import Txoriene from './sorteos/txoriene.jpg';
 import Viñedo from './sorteos/vinedo.jpg';
 import Casarural from  './sorteos/casarural.jpg';
-
-
+import Zumeta from './zumeta.jpg';
+import Txopitea from './txopitea.jpg';
+import Salazar from './salazar.jpg';
+import Sistiaga from './sistiaga.jpg';
+import Lazkano from './lazkano.jpg';
+import Lekue from './lekue.jpg';
+import Jauregi from './jauregi.jpg';
+import Gorriti from './gorriti.jpg';
+import Etxebarria from './etxebarria.jpg';
+import Gernika from './gernika.jpg';
+import Lote1 from './lote1.jpg';
+import Lote2 from './lote2.jpg';
+import Lote3 from './lote3.jpg';
+import Lote4 from './lote4.jpg';
+import Lote5 from './lote5.jpg';
+import Lote6 from './lote6.jpg';
+import Lote7 from './lote7.jpg';
+import Lote8 from './lote8.jpg';
+import Lote9 from './lote9.jpg';
+import Lote10 from './lote10.jpg';
+import Lote11 from './lote11.jpg';
+import Lote12 from './lote12.jpg';
+import Lote13 from './lote13.jpg';
+import Lote14 from './lote14.jpg';
 const images = {
   "novedades4.png": Novedad1,
   "novedades3.png": Novedad2,
@@ -19,6 +41,30 @@ const images = {
   "txoriene.jpg": Txoriene,
   "vinedo.jpg": Viñedo,
   "casarural.jpg": Casarural,
+  "zumeta.jpg": Zumeta,
+  "txopitea.jpg": Txopitea,
+  "salazar.jpg": Salazar,
+  "sistiaga.jpg": Sistiaga,
+  "lazkano.jpg": Lazkano,
+  "lekue.jpg": Lekue,
+  "jauregi.jpg": Jauregi,
+  "gorriti.jpg": Gorriti,
+  "etxebarria.jpg": Etxebarria,
+  "gernika.jpg": Gernika,
+  "lote1.jpg": Lote1,
+  "lote2.jpg": Lote2,
+  "lote3.jpg": Lote3,
+  "lote4.jpg": Lote4,
+  "lote5.jpg": Lote5,
+  "lote6.jpg": Lote6,
+  "lote7.jpg": Lote7,
+  "lote8.jpg": Lote8,
+  "lote9.jpg": Lote9,
+  "lote10.jpg": Lote10,
+  "lote11.jpg": Lote11,
+  "lote12.jpg": Lote12,
+  "lote13.jpg": Lote13,
+  "lote14.jpg": Lote14
 };
 
 export default images;
