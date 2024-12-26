a9253329264c78e4d6fd3031309eca6d75e39116
UxueAS <uxue.anabitarte838@comunidadunir.net>
2024-12-26 13:40:34 +0100

    añadir imagenes

--- src/assets/images.js
+++ src/assets/images.js
@@ -31,6 +31,88 @@ import Lote11 from './lote11.jpg';
 import Lote12 from './lote12.jpg';
 import Lote13 from './lote13.jpg';
 import Lote14 from './lote14.jpg';
+import Libro1 from './libro1.png';
+import Libro2 from './libro2.png';
+import Libro3 from './libro3.png';
+import Libro4 from './libro4.png';
+import Libro5 from './libro5.png';
+import Libro6 from './libro6.png';
+import Libro7 from './libro7.png';
+import Libro8 from './libro8.png';
+import Libro9 from './libro9.png';
+import Libro10 from './libro10.png';
+import Libro11 from './libro11.png';
+import Libro12 from './libro12.png';
+import Libro13 from './libro13.png';
+import Libro14 from './libro14.png';
+import Libro15 from './libro15.png';
+import Libro16 from './libro16.png';
+import Libro17 from './libro17.png';
+import Libro18 from './libro18.png';
+import Libro19 from './libro19.png';
+import Libro20 from './libro20.png';
+import Libro21 from './libro21.png';
+import Libro22 from './libro22.png';
+import Libro23 from './libro23.png';
+import Libro24 from './libro24.png';
+import Libro25 from './libro25.png';
+import Libro26 from './libro26.png';
+import Libro27 from './libro27.png';
+import Libro28 from './libro28.png';
+import Libro29 from './libro29.png';
+import Libro30 from './libro30.png';
+import Libro31 from './libro31.png';
+import Libro32 from './libro32.png';
+import Libro33 from './libro33.png';
+import Libro34 from './libro34.png';
+import Libro35 from './libro35.png';
+import Libro36 from './libro36.png';
+import Libro37 from './libro37.png';
+import Libro38 from './libro38.png';
+import Libro39 from './libro39.png';
+import Libro40 from './libro40.png';
+import Libro41 from './libro41.png';
+import Libro42 from './libro42.png';
+import Libro43 from './libro43.png';
+import Libro44 from './libro44.png';
+import Libro45 from './libro45.png';
+import Anuario1 from './anuario1.png';
+import Anuario2 from './anuario2.png';
+import Anuario3 from './anuario3.png';
+import Anuario4 from './anuario4.png';
+import Anuario5 from './anuario5.png';
+import Anuario6 from './anuario6.png';
+import Anuario7 from './anuario7.png';
+import Anuario8 from './anuario8.png';
+import Anuario9 from './anuario9.png';
+import Anuario10 from './anuario10.png';
+import Cd1 from './cd1.png';
+import Cd2 from './cd2.png';
+import Cd3 from './cd3.png';
+import Cd4 from './cd4.png';
+import Cd5 from './cd5.png';
+import Dvd1 from './dvd1.png';
+import Dvd2 from './dvd2.png';
+import Dvd3 from './dvd3.png';
+import Dvd4 from './dvd4.png';
+import Dvd5 from './dvd5.png';
+import Guia1 from './guia1.png';
+import Guia2 from './guia2.png';
+import Guia3 from './guia3.png';
+import Guia4 from './guia4.png';
+import Guia5 from './guia5.png';
+import Guia6 from './guia6.png';
+import Guia7 from './guia7.png';
+import Guia8 from './guia8.png';
+import Guia9 from './guia9.png';
+import Guia10 from './guia10.png';
+import Guia11 from './guia11.png';
+import Guia12 from './guia12.png';
+import Guia13 from './guia13.png';
+import Guia14 from './guia14.png';
+import Guia15 from './guia15.png';
+import Guia16 from './guia16.png';
+
 const images = {
   "novedades4.png": Novedad1,
   "novedades3.png": Novedad2,
@@ -64,7 +146,88 @@ const images = {
   "lote11.jpg": Lote11,
   "lote12.jpg": Lote12,
   "lote13.jpg": Lote13,
-  "lote14.jpg": Lote14
+  "lote14.jpg": Lote14,
+  "libro1.png": Libro1,
+  "libro2.png": Libro2,
+  "libro3.png": Libro3,
+  "libro4.png": Libro4,
+  "libro5.png": Libro5,
+  "libro6.png": Libro6,
+  "libro7.png": Libro7,
+  "libro8.png": Libro8,
+  "libro9.png": Libro9,
+  "libro10.png": Libro10,
+  "libro11.png": Libro11,
+  "libro12.png": Libro12,
+  "libro13.png": Libro13,
+  "libro14.png": Libro14,
+  "libro15.png": Libro15,
+  "libro16.png": Libro16,
+  "libro17.png": Libro17,
+  "libro18.png": Libro18,
+  "libro19.png": Libro19,
+  "libro20.png": Libro20,
+  "libro21.png": Libro21,
+  "libro22.png": Libro22,
+  "libro23.png": Libro23,
+  "libro24.png": Libro24,
+  "libro25.png": Libro25,
+  "libro26.png": Libro26,
+  "libro27.png": Libro27,
+  "libro28.png": Libro28,
+  "libro29.png": Libro29,
+  "libro30.png": Libro30,
+  "libro31.png": Libro31,
+  "libro32.png": Libro32,
+  "libro33.png": Libro33,
+  "libro34.png": Libro34,
+  "libro35.png": Libro35,
+  "libro36.png": Libro36,
+  "libro37.png": Libro37,
+  "libro38.png": Libro38,
+  "libro39.png": Libro39,
+  "libro40.png": Libro40,
+  "libro41.png": Libro41,
+  "libro42.png": Libro42,
+  "libro43.png": Libro43,
+  "libro44.png": Libro44,
+  "libro45.png": Libro45,
+  "anuario1.png": Anuario1,
+  "anuario2.png": Anuario2,
+  "anuario3.png": Anuario3,
+  "anuario4.png": Anuario4,
+  "anuario5.png": Anuario5,
+  "anuario6.png": Anuario6,
+  "anuario7.png": Anuario7,
+  "anuario8.png": Anuario8,
+  "anuario9.png": Anuario9,
+  "anuario10.png": Anuario10,
+  "cd1.png": Cd1,
+  "cd2.png": Cd2,
+  "cd3.png": Cd3,
+  "cd4.png": Cd4,
+  "cd5.png": Cd5,
+  "dvd1.png": Dvd1,
+  "dvd2.png": Dvd2,
+  "dvd3.png": Dvd3,
+  "dvd4.png": Dvd4,
+  "dvd5.png": Dvd5,
+  "guia1.png": Guia1,
+  "guia2.png": Guia2,
+  "guia3.png": Guia3,
+  "guia4.png": Guia4,
+  "guia5.png": Guia5,
+  "guia6.png": Guia6,
+  "guia7.png": Guia7,
+  "guia8.png": Guia8,
+  "guia9.png": Guia9,
+  "guia10.png": Guia10,
+  "guia11.png": Guia11,
+  "guia12.png": Guia12,
+  "guia13.png": Guia13,
+  "guia14.png": Guia14,
+  "guia15.png": Guia15,
+  "guia16.png": Guia16
 };
 
 export default images;
