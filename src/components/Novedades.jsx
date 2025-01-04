160551c2073b0b1d7b19c03cf1981f5728840fae
UxueAS <uxue.anabitarte838@comunidadunir.net>
2025-01-04 10:24:28 +0100

    productos novedades

--- src/components/Novedades.jsx
+++ src/components/Novedades.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Novedades = () => {
   const [novedades, setNovedades] = useState([]);
   useEffect(() => {
-    fetch('https://dummyjson.com/c/ba4f-47e4-4ceb-8beb')
+    fetch(`${import.meta.env.VITE_API_URL}/novedades`)
       .then(response => response.json())
       .then(data => setNovedades(data));
   }, []);
@@ -16,8 +16,8 @@ const Novedades = () => {
           {novedades.map(product => (
             <div key={product.id} className='flex flex-col'>
               <img src={images[product.img]} alt={product.title} className='w-full aspect-square' />
-              <Link to={`/product/${product.id}`}>
-                <h3 className='font-semibold text-2xl'>{product.title}</h3>
+              <Link to={ product.type == 'producto' ? `/productos/${product.id}` : `/sorteos/${product.id}`}>  
+                <h3 className='font-semibold text-2xl w-full break-words'>{product.title}</h3>
               </Link>
               <p className='text-lg'>{product.text}</p>
             </div>
